0795dbd8b5d65bc96d560449a3b03fe977890b5e
TadMur <tats.adv.util@gmail.com>
2019-05-19 15:40:57 -0500

    17 Mapping Web

---
+++ 17-Mapping-Web/Instructions/Task/static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoidGFkbXVyIiwiYSI6ImNqdmJlMzcxZDBmNDgzeXA2emxscHpmdHkifQ.OY8a20OQAgyenNXID-jzLA";
---
+++ 17-Mapping-Web/Instructions/Task/static/js/logic.js
@@ -0,0 +1,89 @@
+// Creating map object
+var myMap = L.map("map", {
+    center: [39.605688,-100.525781],
+    zoom: 5
+});
+
+// Adding tile layer
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+}).addTo(myMap);
+
+// retrieve earthquake geoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson"), function(data) {
+
+    function () {
+        return {
+            opacity: 1,
+            fillOpacity:1,
+            fillColor: getColor(feature.properties.mag),
+            radius: getRadius(feature.properties.mag),
+            stroke: true,
+            weight: 0.5
+        };
+    }
+    
+    
+    // Define the color of the marker based on the magnitude of the earthquake.
+  function getColor() {
+    switch (true) {
+      case magnitude > 5:
+        return "#ea2c2c";
+      case magnitude > 4:
+        return "#ea822c";
+      case magnitude > 3:
+        return "#ee9c00";
+      case magnitude > 2:
+        return "#eecc00";
+      case magnitude > 1:
+        return "#d4ee00";
+      default:
+        return "#98ee00";
+    };
+  }
+    
+
+    
+    
+
+    
+    
+//--------------------------------
+
+  var legend = L.control({
+    position: "bottomright"
+  });
+
+
+  legend.onAdd = function() {
+    var div = L
+      .DomUtil
+      .create("div", "info legend");
+
+    var grades = [0, 1, 2, 3, 4, 5];
+    var colors = [
+      "#98ee00",
+      "#d4ee00",
+      "#eecc00",
+      "#ee9c00",
+      "#ea822c",
+      "#ea2c2c"
+    ];
+
+
+    for (var i = 0; i < grades.length; i++) {
+      div.innerHTML += "<i style='background: " + colors[i] + "'></i> " +
+        grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+    }
+    return div;
+  };
+
+
+  legend.addTo(map);   
+    
+    
+}
+
